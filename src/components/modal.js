2d84f31b8fb0c8a039fad757e8fc9e40dfe7134d
Anna <annabarzunova@gmail.com>
2024-12-04 11:15:21 +0300

    finals

--- src/components/modal.js
+++ src/components/modal.js
@@ -19,15 +19,11 @@ function closePopup(popup) {
 }
 
 /* Функция закрытия попапов c помощью оверлея */
-// function closePopupByClick(evt) {
-//   const openedPopup = document.querySelector(".popup_is-opened");
-//   if (evt.target.matches(".popup_is-opened, .popup__close"))
-//     closePopup(openedPopup);
-// }
+
 function closePopupByClick(evt) {
-  const openedPopup = document.querySelector(".popup_is-opened");
-  if (evt.target.matches(".popup_is-opened, .popup__close"))
-    closePopup(openedPopup);
+  if (evt.target.matches(".popup_is-opened, .popup__close"));
+    closePopup(evt.target.closest(".popup_is-opened"));
 }
+
 
 export { openPopup, closeByEscape, closePopup, closePopupByClick };
